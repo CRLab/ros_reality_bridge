fe9f91213a0ebfee7669cc9335c40543ab39357d
David Watkins <djrival7@gmail.com>
2019-02-26 16:48:21 -0500

    Added spin to test script

--- scripts/test_server.py
+++ scripts/test_server.py
@@ -15,3 +15,5 @@ plan_grasp_service = rospy.Service("/plan_grasp", ros_reality_bridge.srv.PlanGra
 scene_publisher = rospy.Publisher("/completed_meshes", ros_reality_bridge.msg.MeshArray, queue_size=10)
 ungraspable_mesh_publisher = rospy.Publisher("/ungraspable_scene_meshes", ros_reality_bridge.msg.MeshArray, queue_size=10)
 
+while not rospy.is_shutdown():
+    rospy.spin()
